b4b6c2771fb6e3cb7b5465b20f3f62137cf107c2
Miguel Silva <miguelsilva170393@gmail.com>
2022-09-30 23:41:26 +0100

    Scroll Magic and Gsap lib

---
+++ app.js
@@ -0,0 +1,9 @@
+let controller;
+let SlideScene;
+
+function animateSlides(){
+    //Init Controller
+    controller = new ScrollMagic.Controller();
+    //Select some things
+    const sliders = document.querySelectorAll(".slides");
+}
