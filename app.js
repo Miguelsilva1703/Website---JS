0c15de638062c5d953da6289a517be1b341eabe4
Miguel Silva <miguelsilva170393@gmail.com>
2022-09-30 23:50:28 +0100

    Gsap animation draft(slides)

--- app.js
+++ app.js
@@ -5,5 +5,19 @@ function animateSlides(){
     //Init Controller
     controller = new ScrollMagic.Controller();
     //Select some things
-    const sliders = document.querySelectorAll(".slides");
+    const sliders = document.querySelectorAll(".slide");
+    const nav = document.querySelector(".nav-header");
+    //Loop over each Slide
+    sliders.forEach(slide => {
+        const revealImg = slide.querySelector(".reveal-img");
+        const img = slide.querySelector("img");
+        const revealText = slide.querySelector(".reveal-text");
+        //GSAP
+        gsap.to(revealImg,1,{ x: "100%" });
+    })
 }
+
+
+
+
+animateSlides();
