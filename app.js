9256e55e77e347a7ff3083c5983b3eb60cd671e5
Miguel Silva <miguelsilva170393@gmail.com>
2022-09-30 23:59:36 +0100

    Gsap slide anim draftv2

--- app.js
+++ app.js
@@ -13,7 +13,10 @@ function animateSlides(){
         const img = slide.querySelector("img");
         const revealText = slide.querySelector(".reveal-text");
         //GSAP
-        gsap.to(revealImg,1,{ x: "100%" });
+        const slideTL = gsap.timeline({defaults: {duration: 1, ease: "power2.inOut" }  });
+        slideTL.fromTo(revealImg, {x: "0%"}, {x: "100%"});
+        slideTL.fromTo(img, {x: "0%"}, {x: "100%"});
+        slideTL.fromTo(revealImg, {x: "0%"}, {x: "100%"});
     })
 }
 
