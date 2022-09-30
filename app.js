7dab3db8835e07ca6d68d26e0744badd90b09f51
Miguel Silva <miguelsilva170393@gmail.com>
2022-10-01 00:56:59 +0100

    Slider and Page anims| reveal img & pin bugfix

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let controller;
-let SlideScene;
+let slideScene;
+let pageScene;
 
 function animateSlides(){
     //Init Controller
@@ -8,16 +9,47 @@ function animateSlides(){
     const sliders = document.querySelectorAll(".slide");
     const nav = document.querySelector(".nav-header");
     //Loop over each Slide
-    sliders.forEach(slide => {
+    sliders.forEach((slide, index, slides) => {
         const revealImg = slide.querySelector(".reveal-img");
         const img = slide.querySelector("img");
         const revealText = slide.querySelector(".reveal-text");
-        //GSAP
-        const slideTL = gsap.timeline({defaults: {duration: 1, ease: "power2.inOut" }  });
-        slideTL.fromTo(revealImg, {x: "0%"}, {x: "100%"});
-        slideTL.fromTo(img, {x: "0%"}, {x: "100%"});
-        slideTL.fromTo(revealImg, {x: "0%"}, {x: "100%"});
-    })
+        //GSAP timeline
+        const slideTl = gsap.timeline({defaults: {duration: 1, ease: "power2.inOut" }  });
+        slideTl.fromTo(revealImg, {x: "0%"}, {x: "100%"});
+        slideTl.fromTo(img, {scale: 2}, {scale: 1}, "-=1");
+        slideTl.fromTo(revealText, {x: "0%"}, {x: "100%"}, "-=0.75");
+        slideTl.fromTo(nav, {y: "-100%"}, {y: "0%"}, "-=0.5");
+        //Create Scene
+        slideScene = new ScrollMagic.Scene({
+            triggerElement: slide,
+            triggerHook: 0.25
+        })
+        //Add timeline to tween
+        .setTween(slideTl)
+        //add visual indicator for slide
+        .addIndicators({colorStart: 'white', colorTrigger: "white", name: "slide"})
+        //Hook gsap with scroll magic
+        .addTo(controller);
+        //New animation
+        const pageTl = gsap.timeline();
+        let nextSlide = slides.lenght - 1 === index ? 'end' : slides[index + 1 ];
+        pageTl.fromTo(nextSlide, { y: "0%"}, { y: "50%"});
+        pageTl.fromTo(slide, { opacity: 1, scale: 1 }, {opacity: 0, scale: 0.5})
+        pageTl.fromTo(nextSlide, { y: "50%"}, { y: "0%"}, "-=0.5");
+        //Create New Scene
+        pageScene = new ScrollMagic.Scene({
+            triggerElement: slide,
+            duration: "100%",
+            triggerHook: 0
+        })
+        //add visual indicator for page
+        .addIndicators({colorStart: 'white', colorTrigger: "white", name: "page", indent: 200})
+        // Add pin to keep img in place and pushes follow up slides(followers) up
+        .setPin(slide, { pushFollowers: false})
+        .setTween(pageTl)
+        //Hook gsap with scroll magic
+        .addTo(controller);
+    });
 }
 
 
