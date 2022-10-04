d56517c318dbd6d57434b808b28f5bf07d439681
Miguel Silva <miguelsilva170393@gmail.com>
2022-10-04 11:09:07 +0100

    Adding Scroll magic to Fashion page

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let controller;
 let slideScene;
 let pageScene;
+let detailScene;
 const mouse = document.querySelector(".cursor");
 const mouseTxt = mouse.querySelector("span");
 const burguer = document.querySelector(".burguer");
@@ -120,6 +121,12 @@ barba.init({
             namespace: "fashion",
             beforeEnter(){
                 logo.href= "../index.html"
+                detailAnimation();
+                gsap.fromTo(".nav-header", 1, {y: "100%"}, {y: "0%", ease:"power2.InOut"})
+            },
+            beforeLeave(){
+                controller.destroy();
+                detailScene.destroy();
             }
         }
     ],
@@ -147,6 +154,30 @@ barba.init({
     ]
 })
 
+
+
+
+function detailAnimation(){
+    controller = new ScrollMagic.Controller();
+    const slides = document.querySelectorAll((".detail-slide"))
+    slides.forEach((slide, index, slides) => {
+        const slideTl = gsap.timeline({defaults: {duration:1}})
+        let nextSlide = slides.lenght - 1 === index ? 'end' : slides[index + 1 ];
+        const nextImg = nextSlide.querySelector('img');
+        slideTl.fromTo(slide, { opacity: 1 }, { opacity: 0 });
+        slideTl.fromTo(nextSlide, { opacity : 0}, { opacity: 1}, "-=1" );
+        slideTl.fromTo(nextImg, {x:"50%"}, {x: "0%"})
+        //Scene
+        detailScene = new ScrollMagic.Scene({
+            triggerElement: slide,
+            duration: '100%',
+            triggerHook: 0
+        }).setPin(slide, { pushFollowers: false })
+        .setTween(slideTl)
+        .addIndicators({colorStart: 'white', colorTrigger: "white", name: "detailscene"})
+        .addTo(controller)
+    });
+}
 
 
 
